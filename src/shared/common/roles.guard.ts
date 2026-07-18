d7681b842edd3ef567e3cb91aed9a60e648248da
Tran Quang Ha <tranquangha2006@gmail.com>
2026-07-18 22:39:28 +0700

    feat: add role guard

---
+++ src/shared/common/roles.guard.ts
@@ -0,0 +1,27 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { RequestWithUser } from './jwt.payload.interface';
+import { Roles } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get(Roles, context.getHandler());
+    if (!requiredRoles) {
+      throw new ForbiddenException('Route is missing @Roles() metadata');
+    }
+    const { user } = context.switchToHttp().getRequest<RequestWithUser>();
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        'You do not have permission to access this resource',
+      );
+    }
+    return true;
+  }
+}
---
+++ src/shared/domain/enum.ts
@@ -0,0 +1,4 @@
+export enum SystemRole {
+  Admin = 'admin',
+  Employee = 'employee',
+}
