592b6f8f9e4f7d1275f89b9480d795439157d56c
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-15 21:10:20 +0700

    feat: generate document data response type

---
+++ src/modules/document/application/dtos/document.response.dto.ts
@@ -0,0 +1,17 @@
+import { CommonDocumentType } from 'src/shared/domain/enum';
+
+export type DocumentListResponseDto = {
+  publicId: string;
+  title: string;
+  fileType: CommonDocumentType;
+  lastUpdated: Date;
+  category: {
+    publicId: string;
+    name: string;
+  };
+  updatedBy: {
+    publicId: string;
+    name: string;
+    avatarUrl: string | null;
+  };
+};
