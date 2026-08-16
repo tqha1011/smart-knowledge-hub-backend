64df3b3ed78ac7b2199175607141254f02382b44
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-16 12:08:35 +0700

    feat: generate document detail data type

--- src/modules/document/application/dtos/document.response.dto.ts
+++ src/modules/document/application/dtos/document.response.dto.ts
@@ -16,3 +16,26 @@ export type DocumentListResponseDto = {
   };
   cited: number;
 };
+
+export type DocumentDetailResponseDto = {
+  publicId: string;
+  title: string;
+  fileType: CommonDocumentType;
+  fileSize: number;
+  content: string | null;
+  lastUpdated: Date;
+  category: {
+    publicId: string;
+    name: string;
+  };
+  updatedBy: {
+    publicId: string;
+    name: string;
+    avatarUrl: string | null;
+  };
+  citedQuestion: {
+    publicId: string;
+    name: string;
+    lastAsked: Date;
+  }[]; // if doesn't have any cited question, return empty array
+};
