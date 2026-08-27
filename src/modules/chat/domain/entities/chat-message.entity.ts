f8ea4760e66a0ac51c827e2f4ef4f2a62612437e
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-27 14:31:33 +0700

    feat: create chat message entity, domain erros

--- src/modules/chat/domain/entities/chat-message.entity.ts
+++ src/modules/chat/domain/entities/chat-message.entity.ts
@@ -1,7 +1,56 @@
+import { CommonChatRole } from 'src/shared/domain/enum';
+
 export type ChatMessageGetParams = {
   readonly chatSessionId: number;
   readonly messageId: number;
   readonly content: string;
+  readonly role: CommonChatRole;
   readonly createdAt: Date;
   updatedAt: Date;
 };
+
+export type ChatMessageCreateParams = Omit<
+  ChatMessageGetParams,
+  'messageId' | 'createdAt' | 'updatedAt'
+>;
+
+export class ChatMessage {
+  private constructor(private params: ChatMessageGetParams) {}
+
+  static getChatMessage(params: ChatMessageGetParams): ChatMessage {
+    return new ChatMessage(params);
+  }
+
+  static createChatMessage(params: ChatMessageCreateParams): ChatMessage {
+    return new ChatMessage({
+      ...params,
+      messageId: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  get chatSessionId(): number {
+    return this.params.chatSessionId;
+  }
+
+  get messageId(): number {
+    return this.params.messageId;
+  }
+
+  get content(): string {
+    return this.params.content;
+  }
+
+  get role(): CommonChatRole {
+    return this.params.role;
+  }
+
+  get createdAt(): Date {
+    return this.params.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.params.updatedAt;
+  }
+}
