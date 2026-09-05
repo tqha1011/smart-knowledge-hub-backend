72d6c5af22f35a0a32e5816a13d3327ff079b511
Tran Quang Ha <tranquangha2006@gmail.com>
2026-09-05 16:15:33 +0700

    feat: add bullmq dashboard to monitor queue status

--- src/shared/infrastructure/queue/queue.module.ts
+++ src/shared/infrastructure/queue/queue.module.ts
@@ -1,8 +1,13 @@
+import { BullBoardModule } from '@bull-board/nestjs';
+import { ExpressAdapter } from '@bull-board/express';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 import { BullModule } from '@nestjs/bullmq';
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 import { QueueName } from './constant/queue-name';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 @Global()
 @Module({
@@ -28,6 +33,27 @@ import { QueueName } from './constant/queue-name';
         name: QueueName.SendEmailQueue,
       },
     ),
+
+    ...(isProduction
+      ? []
+      : [
+          BullBoardModule.forRoot({
+            route: '/admin/queues',
+            adapter: ExpressAdapter,
+          }),
+          BullBoardModule.forFeature({
+            name: QueueName.IngestionQueue,
+            adapter: BullMQAdapter,
+          }),
+          BullBoardModule.forFeature({
+            name: QueueName.GenerateTitleQueue,
+            adapter: BullMQAdapter,
+          }),
+          BullBoardModule.forFeature({
+            name: QueueName.SendEmailQueue,
+            adapter: BullMQAdapter,
+          }),
+        ]),
   ],
   exports: [BullModule],
 })
